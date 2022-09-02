0abc73e30506d2138b20ac3cd2057d6f700a10f8
Samus <26967164+SamusElderg@users.noreply.github.com>
2022-09-02 22:07:04 +0800

    add mainnet/testnet switcher
    added a simple mainnet/testnet switcher

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,45 +1,59 @@
-import React, { useContext } from 'react'
-import { Link } from "react-router-dom"
-import { Layout, Row, Col } from 'antd';
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { Layout, Row, Col } from "antd";
 
-import Breakpoint from 'react-socks';
+import Breakpoint from "react-socks";
 
-import { Context } from '../../context'
+import { Context } from "../../context";
 
-import { Icon, Button, WalletAddrShort } from '../Components'
+import { Icon, Button, WalletAddrShort } from "../Components";
 
-const Header = (props) => {
+const Header = () => {
+  const context = useContext(Context);
+  const [isTestnet, setisTestnet] = useState(
+    window.localStorage.getItem("STBT_isTestnet")
+  );
 
-  const context = useContext(Context)
+  const toggleMainnet = () => {
+    if (isTestnet) {
+      window.localStorage.removeItem("STBT_isTestnet");
+    } else {
+      window.localStorage.setItem("STBT_isTestnet", "1");
+    }
+    setisTestnet(!isTestnet);
+    window.location.reload();
+  };
 
   return (
-    <Layout.Header className="header-container">
+    <Layout.Header className='header-container'>
       <Row>
-      <Col xs={4}>
-      <Link to="/">
-        <Icon icon="logo" />
-      </Link>
-      </Col>
-      <Col xs={16}>
-      </Col>
-      
         <Col xs={4}>
-        <Breakpoint medium up>
-        {context.wallet ?
-        <Link to="/wallet"><WalletAddrShort /></Link>
-            :
-            <Link to="/wallet/unlock">
-              <Button fill>
-                Connect my Wallet
-              </Button>
-            </Link>
-        }
-              </Breakpoint>
-      </Col>
-
+          <Link to='/'>
+            <Icon icon='logo' />
+          </Link>
+        </Col>
+        <Col xs={12}></Col>
+        <Col xs={4}>
+          <Button onClick={() => toggleMainnet()} fill>
+            {isTestnet ? "Testnet" : "Mainnet"}
+          </Button>
+        </Col>
+        <Col xs={4}>
+          <Breakpoint medium up>
+            {context.wallet ? (
+              <Link to='/wallet'>
+                <WalletAddrShort />
+              </Link>
+            ) : (
+              <Link to='/wallet/unlock'>
+                <Button fill>Connect my Wallet</Button>
+              </Link>
+            )}
+          </Breakpoint>
+        </Col>
       </Row>
     </Layout.Header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/env.js
+++ src/env.js
@@ -1,21 +1,8 @@
-const prod_hostnames = [
-  "beptools.org",
-  "binancetools-prod.firebaseapp.com",
-  "binancetools-prod.web.app",
-  "localhost",
-]
+const isMainnet = !window.localStorage.getItem("STBT_isTestnet");
+const isTestnet = !isMainnet;
+const CHAIN_ID = isTestnet ? "Binance-Chain-Nile" : "Binance-Chain-Tigris";
+const NETWORK_ID = 714;
 
-const isMainnet = prod_hostnames.includes(window.location.hostname)
-const isTestnet = !isMainnet
-const CHAIN_ID = isTestnet ? "Binance-Chain-Nile" : "Binance-Chain-Tigris"
-const NETWORK_ID = 714
+const NET = isTestnet ? "testnet" : "mainnet";
 
-const NET = isTestnet ? "testnet" : "mainnet"
-
-export {
-  NET,
-  CHAIN_ID,
-  NETWORK_ID,
-  isTestnet,
-  isMainnet,
-}
+export { NET, CHAIN_ID, NETWORK_ID, isTestnet, isMainnet };
